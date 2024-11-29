b975af392b8f51789e8ace2b2302d22e8f5be335
Gaohonggg <162142451+Gaohonggg@users.noreply.github.com>
2024-11-30 01:54:44 +0700

    Add files via upload

---
+++ Time_5.py
@@ -0,0 +1,211 @@
+import torch
+import random
+from torch import nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader, random_split
+import torchvision
+from torchvision.datasets import FashionMNIST
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+import numpy as np
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+SEED = 42
+set_seed(SEED)
+
+train_dataset = FashionMNIST("./data/train",
+                             train=True,
+                             download=True,
+                             transform=transforms.ToTensor())
+test_dataset = FashionMNIST("./data/test",
+                             train=False,
+                             download=True,
+                             transform=transforms.ToTensor())
+
+train_ratio = 0.9
+train_size = int(len(train_dataset) * train_ratio)
+val_size = len(train_dataset) - train_size
+
+train_subset, val_subset = random_split(train_dataset, [train_size, val_size])
+
+batch_size = 512
+train_loader = DataLoader(train_subset,
+                          batch_size=batch_size,
+                          shuffle=True)
+val_loader = DataLoader(val_subset,
+                        batch_size=batch_size,
+                        shuffle=False)
+test_loader = DataLoader(test_dataset,
+                         batch_size=batch_size,
+                         shuffle=False)
+
+print(f"Train size: {len(train_subset)}")
+print(f"Validation size: {len(val_subset)}")
+print(f"Test size: {len(test_dataset)}")
+
+
+class MLP(nn.Module):
+    def __init__(self, input_dims, hidden_dims, output_dims):
+        super(MLP, self).__init__()
+        self.flatten = nn.Flatten()
+        self.layer1 = nn.Sequential(
+            nn.Linear(input_dims,hidden_dims),
+            nn.BatchNorm1d(hidden_dims),
+            nn.Sigmoid()
+        )
+        self.layer2 = nn.Sequential(
+            nn.Linear(hidden_dims,hidden_dims),
+            nn.BatchNorm1d(hidden_dims),
+            nn.Sigmoid()
+        )
+        self.layer3 = nn.Sequential(
+            nn.Linear(hidden_dims,hidden_dims),
+            nn.BatchNorm1d(hidden_dims),
+            nn.Sigmoid()
+        )
+        self.layer4 = nn.Sequential(
+            nn.Linear(hidden_dims,hidden_dims),
+            nn.BatchNorm1d(hidden_dims),
+            nn.Sigmoid()
+        )
+        self.layer5 = nn.Sequential(
+            nn.Linear(hidden_dims,hidden_dims),
+            nn.BatchNorm1d(hidden_dims),
+            nn.Sigmoid()
+        )
+        self.layer6 = nn.Sequential(
+            nn.Linear(hidden_dims,hidden_dims),
+            nn.BatchNorm1d(hidden_dims),
+            nn.Sigmoid()
+        )
+        self.layer7 = nn.Sequential(
+            nn.Linear(hidden_dims,hidden_dims),
+            nn.BatchNorm1d(hidden_dims),
+            nn.Sigmoid()
+        )
+        self.output = nn.Linear(hidden_dims,output_dims)
+
+        for module in self.modules():
+            if isinstance(module, nn.Linear):
+                nn.init.normal_(module.weight, mean=0.0, std=0.05)
+                nn.init.constant_(module.bias, 0.0)
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)
+        x = self.layer7(x)
+        x = self.output(x)
+        return x
+
+input_dims = 784
+hidden_dims = 128
+output_dims = 10
+lr = 1e-2
+
+model = MLP(input_dims=input_dims,
+            hidden_dims=hidden_dims,
+            output_dims=output_dims).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=lr)
+
+epochs = 100
+train_loss_lst = []
+train_acc_lst = []
+val_loss_lst = []
+val_acc_lst = []
+
+for epoch in range(epochs):
+    train_loss = 0.0
+    train_acc = 0.0
+    count = 0
+
+    model.train()
+    for X_train, y_train in train_loader:
+        X_train, y_train = X_train.to(device), y_train.to(device)
+        optimizer.zero_grad()
+        outputs = model(X_train)
+        loss = criterion(outputs, y_train)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+        train_acc += (torch.argmax(outputs, 1) == y_train).sum().item()
+        count += len(y_train)
+
+    train_loss /= len(train_loader)
+    train_loss_lst.append(train_loss)
+    train_acc /= count
+    train_acc_lst.append(train_acc)
+
+    val_loss = 0.0
+    val_acc = 0.0
+    count = 0
+    model.eval()
+    with torch.no_grad():
+        for X_val, y_val in val_loader:
+            X_val, y_val = X_val.to(device), y_val.to(device)
+            outputs = model(X_val)
+            loss = criterion(outputs, y_val)
+            val_loss += loss.item()
+            val_acc += (torch.argmax(outputs, 1) == y_val).sum().item()
+            count += len(y_val)
+
+    val_loss /= len(test_loader)
+    val_loss_lst.append(val_loss)
+    val_acc /= count
+    val_acc_lst.append(val_acc)
+
+    print(f"EPOCH {epoch+1}/{epochs}, Train_Loss: {train_loss:.4f}, Train_Acc: {train_acc:.4f}, Validation Loss: {val_loss:.4f}, Val_Acc: {val_acc:.4f}")
+
+fig, ax = plt.subplots(2, 2, figsize=(12, 10))
+ax[0, 0].plot(train_loss_lst, color='green')
+ax[0, 0].set(xlabel='Epoch', ylabel='Loss')
+ax[0, 0].set_title('Training Loss')
+
+ax[0, 1].plot(val_loss_lst, color='orange')
+ax[0, 1].set(xlabel='Epoch', ylabel='Loss')
+ax[0, 1].set_title('Validation Loss')
+
+ax[1, 0].plot(train_acc_lst, color='green')
+ax[1, 0].set(xlabel='Epoch', ylabel='Accuracy')
+ax[1, 0].set_title('Training Accuracy')
+
+ax[1, 1].plot(val_acc_lst, color='orange')
+ax[1, 1].set(xlabel='Epoch', ylabel='Accuracy')
+ax[1, 1].set_title('Validation Accuracy')
+
+plt.show()
+
+val_target = []
+val_predict = []
+
+model.eval()
+with torch.no_grad():
+    for X_val, y_val in val_loader:
+        X_val = X_val.to(device)
+        y_val = y_val.to(device)
+        outputs = model(X_val)
+
+        val_predict.append(outputs.cpu())
+        val_target.append(y_val.cpu())
+
+    val_predict = torch.cat(val_predict)
+    val_target = torch.cat(val_target)
+    val_acc = (torch.argmax(val_predict, 1) == val_target).sum().item() / len(val_target)
+
+    print('Evaluation on val set:')
+    print(f'Accuracy: {val_acc}')
